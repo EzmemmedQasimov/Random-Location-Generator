8980115ae401e1073e3499144383dbcf0d15cf38
EzmemmedQasimov <63806411+EzmemmedQasimov@users.noreply.github.com>
2022-12-15 16:34:19 +0400

    first commit

---
+++ src/CustomMap.ts
@@ -0,0 +1,39 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 2,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
+}
